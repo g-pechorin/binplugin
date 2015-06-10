afc0ddd27a1c1f84a45504c1dfa0fecb00190aee
Peter LaValle <peter.lavalle@gmail.com>
2015-06-10 12:28:04 +0100

    Update foober.cpp

--- plugin/src/foober.cpp
+++ plugin/src/foober.cpp
@@ -1,5 +1,17 @@
-#include "foober.hpp"
+#if !defined(EXPORT_API)
+#	if _MSC_VER // this is defined when compiling with Visual Studio
+#		define EXPORT_API extern "C" __declspec(dllexport) // Visual Studio needs annotating exported functions with this
+#	else
+#		define EXPORT_API extern "C" // XCode does not need annotating exported functions, so define is empty
+#	endif
+#endif
 
+// declare the function (may be redundant)
+EXPORT_API float foo(void);
+
+// ...
+
+// implement the function
 EXPORT_API float foo(void)
 {
 	return 3.14f;
