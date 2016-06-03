70798d28f804f8af85535d85b39df2831637da23
Peter A LaValle <peter.lavalle@nottingham.ac.uk>
2016-06-03 18:54:45 +0100

    OpenCV and Unity5
    ... a very "intricate" demo of Unity5 using OpenCV, via a native DLL.
    
    Specifically - Unity pulls a whole file into a byte array, then, passes the file bytes (and an output buffer)  to a C++ function which uses OpenCV's `cv::imread` to decode the pixel data stored in the bytes into the output buffer. Finally, back in C#, Unity transforms the bytes to a Texture2D instance and uses it to cover the GameObject

--- unity5/unity_plugin_with_opencv/src/unity_plugin_with_opencv.hpp
+++ unity5/unity_plugin_with_opencv/src/unity_plugin_with_opencv.hpp
@@ -1,8 +1,7 @@
+
 /*
 	This macro tells the compiler to create the function in such a way that Unity (or Unity's P/Invoke) can find it in the DLL
 */
-
-
 #if !defined(EXPORT_API)
 #	if _MSC_VER // this is defined when compiling with Visual Studio
 #		define EXPORT_API extern "C" __declspec(dllexport) // Visual Studio needs annotating exported functions with this
@@ -10,3 +9,5 @@
 #		define EXPORT_API extern "C" // XCode does not need annotating exported functions, so define is empty
 #	endif
 #endif
+
+
